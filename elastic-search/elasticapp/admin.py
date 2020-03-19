d310c911bbf88a8fe07e03d93931c55af4d57291
Modistabox <raju@modistabox.com>
2020-03-19 11:09:44 +0530

    add import in admin panel

--- elastic-search/elasticapp/admin.py
+++ elastic-search/elasticapp/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from .models import Post, Banks, Branches
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+
+
+from import_export.admin import ImportExportActionModelAdmin
+
+# class BookAdmin(ImportExportActionModelAdmin):
+#     pass
+#
+from import_export import resources
+
+class BookResource(resources.ModelResource):
+
+    class Meta:
+        model = Banks
+
+
+class BookAdmin(ImportExportModelAdmin):
+    resource_class = BookResource
 
 
 admin.site.register(Post)
 
-admin.site.register(Banks)
+admin.site.register(Banks, BookAdmin)
 
 admin.site.register(Branches)
